f87a0b8274ef6aead87ce4569ea8581e3439d25b
Susan <khatri.susan123@gmail.com>
2021-03-01 02:56:38 +0100

    added service page

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ function init() {
   fetch("https://gai.skmmd.dk/wp-json/wp/v2/home_page?_embed")
     .then((initial) => initial.json())
     .then(callBack);
+
+  fetch("https://gai.skmmd.dk/wp-json/wp/v2/services?_embed")
+    .then((initial) => initial.json())
+    .then(callBack2);
 }
 
 function callBack(data) {
@@ -24,6 +28,26 @@ function showService(singleService) {
 
   const elmain = document.querySelector(".infos-grid");
   elmain.appendChild(clone);
+}
+
+function callBack2(data2) {
+  console.log(data2);
+  data2.forEach(showService2);
+}
+
+function showService2(singleService2) {
+  console.log(singleService2);
+
+  console.log(singleService2);
+  const template2 = document.querySelector("template").content;
+  const clone2 = template2.cloneNode(true);
+  const img_url = singleService2._embedded["wp:featuredmedia"][0].source_url;
+  clone2.querySelector("h1").textContent = singleService2.title.rendered;
+  clone2.querySelector("p").innerHTML = singleService2.content.rendered;
+  clone2.querySelector(".image2").src = img_url;
+
+  const elmain2 = document.querySelector(".columns");
+  elmain2.appendChild(clone2);
 }
 
 const menuI = document.querySelector(".hamMenu");
